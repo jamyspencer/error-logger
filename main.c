2ddc05509b95f8ddb8b8c7fa76ba66a8a03a468f
o-spencer <jamy_spencer@hotmail.com>
2017-01-25 18:33:52 -0600

    switched to using getopt

--- main.c
+++ main.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include "log.h"
+#include <unistd.h>
 
 int main ( int argc, char *argv[] ){
 
 	char* log_file_name = "logfile.txt";
 	int special_number = 37;
+	int c;
 
 	//check that there is the correct number of command line arguments
-	if (argc < 1 || argc > 3){
-		printf("Error: This program requires 0-2 command line arguments\n");
-                return 1;
-	}
-	//command line argument functionality
-	if (strcmp(argv[1], "-h") == 0){
-		printf("%s\n", "-h\tHelp Menu\n-n\tSet special number\n-l\tSet log file name");
-		return 0;
-	}
-	else if (strcmp(argv[1], "-l") == 0){
-		if (argc == 3){
+
+	while ( (c = getopt(argc, argv, "hl:n:012")) != -1) {
+		switch(c){
+		case '0':
+		case '1':
+		case '2':
+		case 'h':
+			printf("%s\n", "-h\tHelp Menu\n-n\tSet special number\n-l\tSet log file name");
+			return 0;
+			break;
+		case 'l':
 			log_file_name = argv[2];
-		}
-		else{
-			printf("Error: when using the -l flag you have to include a file name\n");
-			return 1;
-		}
-	}
-	else if (strcmp(argv[1], "-n") == 0){
-                if (argc == 3){
+			break;
+		case 'n':
                         special_number = atoi(argv[2]);
-                }
-                else{
-                        printf("Error: when using the -n flag you have to include a special number\n");
-                        return 1;
+			break;
                 }
         }
 
-	return 0; 
+	return 0;
 }
